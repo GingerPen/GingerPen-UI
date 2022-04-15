b0f4689c37e2aae4a2f44a47caaa2778a83985e4
Harshit Sharma <harshit19csu411@ncuindia.edu>
2022-04-16 01:27:35 +0530

    Created minimalistic layout
    Added Minimalistic layout of Coded Editor and Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GingerPen-UI';
+  code: string = "";
+
+  
+constructor(private http:HttpClient) {
+
 }
+  runCode() {
+    this.http.get('http://localhost:8080/').subscribe(data=>{
+      console.warn(data);
+    });
+
+    this.http.post<any>('http://localhost:8080/runCode', { language:"java" , code: this.code }).subscribe(data => {
+      console.log(data);
+  })
+    
+  }
+
+
+
+
+
+}
+
+
